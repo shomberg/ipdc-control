ad3efb7d108b1f1d59ee95ae8b67d1cfd77ff545
Steven Homberg <s.homberg@verizon.net>
2015-05-04 12:23:38 -0400

    Added JOpenShowVar functionality to interface Node

--- rosjava_ws/src/kuka_interface/kuka_action/src/main/java/com/github/kuka_interface/kuka_action/PTPServer.java
+++ rosjava_ws/src/kuka_interface/kuka_action/src/main/java/com/github/kuka_interface/kuka_action/PTPServer.java
@@ -6,9 +6,18 @@ import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
 import org.ros.node.service.ServiceResponseBuilder;
 import org.ros.node.service.ServiceServer;
-//import org.ros.rosjava.kuka_msgs;
+import no.hials.crosscom.CrossComClient;
+import no.hials.crosscom.KRL.structs.KRLPos;
 
 public class PTPServer extends AbstractNodeMain {
+
+    private static final double XTHRESH = .1;
+    private static final double YTHRESH = .1;
+    private static final double ZTHRESH = .1;
+    private static final double ATHRESH = .1;
+    private static final double BTHRESH = .1;
+    private static final double CTHRESH = .1;
+    private CrossComClient client;
 
     @Override
     public GraphName getDefaultNodeName() {
@@ -16,10 +25,37 @@ public class PTPServer extends AbstractNodeMain {
     }
 
     public void onStart(ConnectedNode connectedNode) {
+	try{
+	    client = new CrossComClient("172.31.1.147", 7000);
+	} catch(Exception e){
+	    e.printStackTrace();
+	}
+	KRLPos pos = new KRLPos("MYPOS");
+	KRLE6Pos pos_act = new KRLE6Pos("$POS_ACT");
 	connectedNode.newServiceServer("move_ptp", kuka_msgs.MovePTP._TYPE,new ServiceResponseBuilder<kuka_msgs.MovePTPRequest, kuka_msgs.MovePTPResponse>() {
-		public void
-		    build(kuka_msgs.MovePTPRequest request, kuka_msgs.MovePTPResponse response) {
-		    response.setSuccess(true);
+		public void build(kuka_msgs.MovePTPRequest request, kuka_msgs.MovePTPResponse response) {
+		    pos.setX(request.getX());
+		    pos.setY(request.getY());
+		    pos.setZ(request.getZ());
+		    pos.setA(request.getA());
+		    pos.setB(request.getB());
+		    pos.setC(request.getC());
+		    long start = System.currentTimeMillis();
+		    while(System.currentTimeMillis() - start < TIMEOUT){
+			client.readVariable(pos_act);
+			double xOff = Math.abs(pos_act.getX() - request.getX());
+			double yOff = Math.abs(pos_act.getY() - request.getY());
+			double zOff = Math.abs(pos_act.getZ() - request.getZ());
+			double aOff = Math.abs(pos_act.getA() - request.getA());
+			double bOff = Math.abs(pos_act.getB() - request.getB());
+			double cOff = Math.abs(pos_act.getC() - request.getC());
+			if(xOff < XTHRESH && yOff < YTHRESH && zOff < ZTHRESH
+			   && aOff < ATHRESH && bOff < BTHRESH && cOff < CTHRESH){
+			    response.setSuccess(true);
+			    return;
+			}
+		    }
+		    response.setSuccess(false);
 		}
 	    });
     }
